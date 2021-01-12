73469f862c714aa3a2201f66d302ee8390f7df99
Maciej Zielinski <maciek.s.zielinski@gmail.com>
2021-01-12 09:36:29 +0100

    wip

--- client/src/keys-manager.js
+++ client/src/keys-manager.js
@@ -51,47 +51,47 @@ let secondAccount = masterKey.deriveIndex(2);
     // Deployment Threshold should be set to 1.
     // Key Management Threshold should be set to 1.
     console.log("\n0. Initial settings.\n");
-    await printAccount();
+    // await printAccount();
     
     // 1. Set faucet's weight to 3
     console.log("\n1. Set faucet's weight to 3\n");
     deploy = buildSetKeyWeightDeploy(faucetAccount, 3);
     await sendDeploy(deploy, faucetAccount, [faucetAccount]);
 
-    // 2. Set Keys Management Threshold to 3.
-    console.log("\n2. Set Keys Management Threshold to 3\n");
-    deploy = buildSetKeyManagementThresholdDeploy(3);
-    await sendDeploy(deploy, faucetAccount, [faucetAccount]);
+    // // 2. Set Keys Management Threshold to 3.
+    // console.log("\n2. Set Keys Management Threshold to 3\n");
+    // deploy = buildSetKeyManagementThresholdDeploy(3);
+    // await sendDeploy(deploy, faucetAccount, [faucetAccount]);
 
-    // 3. Set Deploy Threshold to 2.
-    console.log("\n3. Set Deploy Threshold to 2.\n");
-    deploy = buildSetDeploymentThresholdDeploy(2);
-    await sendDeploy(deploy, faucetAccount, [faucetAccount]);
+    // // 3. Set Deploy Threshold to 2.
+    // console.log("\n3. Set Deploy Threshold to 2.\n");
+    // deploy = buildSetDeploymentThresholdDeploy(2);
+    // await sendDeploy(deploy, faucetAccount, [faucetAccount]);
     
-    // 4. Add first new key with weight 1.
-    console.log("\n4. Add first new key with weight 1.\n");
-    deploy = buildSetKeyWeightDeploy(firstAccount, 1);
-    await sendDeploy(deploy, faucetAccount, [faucetAccount]);
+    // // 4. Add first new key with weight 1.
+    // console.log("\n4. Add first new key with weight 1.\n");
+    // deploy = buildSetKeyWeightDeploy(firstAccount, 1);
+    // await sendDeploy(deploy, faucetAccount, [faucetAccount]);
     
-    // 5. Add second new key with weight 1.
-    console.log("\n5. Add second new key with weight 1.\n");
-    deploy = buildSetKeyWeightDeploy(secondAccount, 1);
-    await sendDeploy(deploy, faucetAccount, [faucetAccount]);
+    // // 5. Add second new key with weight 1.
+    // console.log("\n5. Add second new key with weight 1.\n");
+    // deploy = buildSetKeyWeightDeploy(secondAccount, 1);
+    // await sendDeploy(deploy, faucetAccount, [faucetAccount]);
     
-    // 6. Make a transfer from faucet using only both accounts.
-    console.log("\n6. Make a transfer from faucet using only both accounts.\n");
-    deploy = buildTransferDeploy(faucetAccount, firstAccount, 1);
-    await sendDeploy(deploy, faucetAccount, [firstAccount, secondAccount]);
+    // // 6. Make a transfer from faucet using only both accounts.
+    // console.log("\n6. Make a transfer from faucet using only both accounts.\n");
+    // deploy = buildTransferDeploy(faucetAccount, firstAccount, 1);
+    // await sendDeploy(deploy, faucetAccount, [firstAccount, secondAccount]);
 
-    // 7. Remove first account.
-    console.log("\n7. Remove the first account\n");
-    deploy = buildSetKeyWeightDeploy(firstAccount, 0);
-    await sendDeploy(deploy, faucetAccount, [faucetAccount]);
+    // // 7. Remove first account.
+    // console.log("\n7. Remove the first account\n");
+    // deploy = buildSetKeyWeightDeploy(firstAccount, 0);
+    // await sendDeploy(deploy, faucetAccount, [faucetAccount]);
 
-    // 8. Remove second account.
-    console.log("\n8. Remove the second account\n");
-    deploy = buildSetKeyWeightDeploy(secondAccount, 0);
-    await sendDeploy(deploy, faucetAccount, [faucetAccount]);
+    // // 8. Remove second account.
+    // console.log("\n8. Remove the second account\n");
+    // deploy = buildSetKeyWeightDeploy(secondAccount, 0);
+    // await sendDeploy(deploy, faucetAccount, [faucetAccount]);
 
 })();
 
@@ -100,7 +100,11 @@ async function sendDeploy(deploy, fromAccount, signingKeys) {
         console.log(`Signed by: ${toAccountHashString(key.publicKey)}`);
         deploy = client.signDeploy(deploy, key);
     }
-    deployHash = await client.putDeploy(deploy);
+    try {
+        deployHash = await client.putDeploy(deploy);
+    } catch (e) {
+        console.log(e);
+    }
     await printDeploy(deployHash);
     await printAccount();
 }
@@ -120,23 +124,23 @@ async function getDeploy(deployHash) {
 
 function buildSetKeyWeightDeploy(account, weight) {
     return buildKeyManagerDeploy(faucetAccount, {
-        action: CLValue.fromString("set_key_weight"),
-        account: CLValue.fromBytes(account.accountHash()),
-        weight: CLValue.fromU8(weight)
+        action: CLValue.string("set_key_weight"),
+        account: CLValue.byteArray(account.accountHash()),
+        weight: CLValue.u8(weight)
     });
 }
 
 function buildSetDeploymentThresholdDeploy(weight) {
     return buildKeyManagerDeploy(faucetAccount, {
-        action: CLValue.fromString("set_deployment_threshold"),
-        weight: CLValue.fromU8(weight)
+        action: CLValue.string("set_deployment_threshold"),
+        weight: CLValue.u8(weight)
     });
 }
 
 function buildSetKeyManagementThresholdDeploy(weight) {
     return buildKeyManagerDeploy(faucetAccount, {
-        action: CLValue.fromString("set_key_management_threshold"),
-        weight: CLValue.fromU8(weight)
+        action: CLValue.string("set_key_management_threshold"),
+        weight: CLValue.u8(weight)
     });
 }
 
@@ -174,8 +178,8 @@ function buildTransferDeploy(fromAccount, toAccount, amount) {
 }
 
 async function printDeploy(deployHash) {
-    console.log("Transfer hash: " + deployHash);
-    console.log("Transfer result:");
+    console.log("Deploy hash: " + deployHash);
+    console.log("Deploy result:");
     console.log(await getDeploy(deployHash));
 }
 
